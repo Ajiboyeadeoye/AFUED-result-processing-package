ac5a362e57d707f15b64dc895a5830b9c16789b2
Ajiboye <ajiboyeadeoye25@yahoo.com>
2025-10-28 10:54:17 +0100

    Payment gateway changed to paystack

--- backend/utils/stripeClient.js
+++
@@ -1,8 +0,0 @@
-// backend/utils/stripeClient.js
-import Stripe from "stripe";
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2023-08-16",
-});
-
-export default stripe;
